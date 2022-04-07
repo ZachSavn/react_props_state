2071ba3ac8e5ed48e75d8c0434cedddcc510c2ca
zachary webb <zachsavn@gmail.com>
2022-04-07 15:04:35 -0400

    Most of exercise completed

--- src/App.jsx
+++ src/App.jsx
@@ -1,23 +1,55 @@
 import "./App.css";
 import { Component } from "react";
-
-let boxes = [];
-let numBoxes = 24;
-
-for (i = 0; i < numBoxes, i++; ) {
-  Push(boxes) => 
-}
+import Box from "./components/Box"
 
 class App extends Component {
   constructor(props) {
     super(props);
 
     // set default state
+    let boxes = [];
+    let numBoxes = 24;
+   let i = 0
+    //iterate through boxes
 
+    for (i = 0, i < numBoxes; i++;) {
+      boxes.push({
+        id: i,
+        color: `rgb(${this.getRandomColor()}, ${this.getRandomColor()}, ${this.getRandomColor()})`
+    }); 
+    }
+    this.state = {
+      boxes
+    };
     // bind methods to this
-  }
+    this.handleBoxClick = this.handleBoxClick.bind(this);
 
+  }
+  handleBoxClick(e){
+    const newBoxes = this.state.boxes.map((box) => {
+      //only change color for specified 1 box
+    if (box.id === e.target.id) {
+     box.color = `rgb(${this.getRandomColor()}, ${this.getRandomColor()}, ${this.getRandomColor()})`
+    }
+    return box;
+    });
+    this.setState({boxes: newBoxes})
+  }
+  //random color assignment
+  getRandomColor() {
+    const rgb = (Math.round(Math.random() * 255));
+      return rgb;
+  }
   render() {
+     //iterates over boxes and assigns props
+     const renderBoxes = this.state.boxes.map((box) => (
+      <Box
+        key={box.id}
+        id={box.id}
+        color={box.color}
+        handleClick={this.handleBoxClick}
+      />
+    ));
     return (
       <main
         style={{
--- src/components/Box.jsx
+++ src/components/Box.jsx
@@ -2,16 +2,19 @@ import * as React from "react";
 
 const Box = (props) => {
   return (
-    <div
+    <div 
       style={{
         width: "180px",
         height: "180px",
         backgroundColor: props.color,
         border: "1px solid black",
         display: "inline-block",
+        backgroundColor: "prop.color"
       }}
-    >
-      <span>{/* Add rgb value from props */}</span>
+      onClick = {props.handleClick}
+      id = {props.id}
+    > {/* Adds rgb value from props*/}
+      <span>{props.color}</span>
     </div>
   );
 };
